07ff90e0b45125b88499f7b22428ef06f588511e
Radosław Osiński <Radoslaw.Osinski@cwsfe.pl>
2014-07-20 11:55:13 +0200

    Optimize jpg images

--- tools/optimize_jpg.sh
+++ tools/optimize_jpg.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#optimizes jpg images
+#optimizes jpg images for size without loosing image quality
 for i in `find ./ -name \*.jpg`; do
 	outputFile=${i%.jpg}_optimized.jpg;
 	jpegtran -optimize $i > $outputFile;
